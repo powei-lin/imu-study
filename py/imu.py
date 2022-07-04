a0e88f3a2c16802f1c9dcb773f33d4b713d48bbc
Powei Lin <poweilin1994@gmail.com>
2022-07-04 12:11:44 -0400

    integral

---
+++ py/imu.py
@@ -0,0 +1,14 @@
+from scipy.spatial.transform import Rotation
+import numpy as np
+
+def init_rotation(acc, g):
+    v0 = acc
+    v1 = -g
+    unit_vector_1 = v0 / np.linalg.norm(v0)
+    unit_vector_2 = v1 / np.linalg.norm(v1)
+    dot_product = np.dot(unit_vector_1, unit_vector_2)
+    angle = np.arccos(dot_product)
+    rvec = np.cross(v0, v1)
+    rvec = rvec/np.linalg.norm(rvec)*angle
+    rmat = Rotation.from_rotvec(rvec).as_matrix()
+    return rmat
--- py/main.py
+++ py/main.py
@@ -1,36 +1,78 @@
 import json
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
 import pandas as pd
 import numpy as np
 from scipy.spatial.transform import Rotation
 import matplotlib.pyplot as plt
 from pytransform3d.transform_manager import TransformManager
+import imu
 
 
 def main():
     poses = {}
-    with open("out.json") as ifile:
+    with open("imu_data.json") as ifile:
         poses = json.load(ifile)
 
     # load json and draw gt pose
     tm_gt = TransformManager()
+    tm_imu_integral = TransformManager()
+
     white_list = []
-    total_pose_for_show = 250
+    total_pose_for_show = 200
     skip_num = len(poses) // total_pose_for_show
+
+    g = np.array([0, 0, -9.81])
+    prev_timestamp = 0
+    prev_r_w_i = np.eye(3)
+    prev_v_w_i = np.zeros((3, ))
+    prev_p_w_i = np.zeros((3, ))
+    curr_r_w_i = np.eye(3)
+    curr_v_w_i = np.zeros((3, ))
+    curr_p_w_i = np.zeros((3, ))
 
     for i, p in poses.items():
         i = int(i)
+
+        # gt pose
         qx, qy, qz, qw = p['qx'], p['qy'], p['qz'], p['qw']
         px, py, pz = p['px'], p['py'], p['pz']
+        ts_ns = p['time_stamp']
+        a = np.array([p['ax'], p['ay'], p['az']])
+        w = np.array([p['rx'], p['ry'], p['rz']])
         rmat = Rotation.from_quat([qx, qy, qz, qw]).as_matrix()
         pose_mat = np.eye(4)
         pose_mat[:3, :3] = rmat
         pose_mat[:3, 3] = [px, py, pz]
 
+        # imu integration
+        current_pose = np.eye(4)
+        if prev_timestamp == 0:
+            prev_r_w_i = imu.init_rotation(a, g)
+            current_pose[:3, :3] = prev_r_w_i
+            print(prev_r_w_i @ a)
+        else:
+            delta_t = (ts_ns - prev_timestamp) * 1e-9
+
+            delta_t2 = delta_t * delta_t
+            curr_r_w_i = prev_r_w_i @ Rotation.from_rotvec(
+                w * delta_t).as_matrix()
+            delta_v = (g + prev_r_w_i @ a) * delta_t
+            curr_v_w_i = prev_v_w_i + delta_v
+            curr_p_w_i = prev_p_w_i + prev_v_w_i * delta_t + 0.5 * g * delta_t2 + 0.5 * (
+                prev_r_w_i @ a) * delta_t2
+            current_pose[:3, :3] = curr_r_w_i
+            current_pose[:3, 3] = curr_p_w_i
+            prev_r_w_i = curr_r_w_i.copy()
+            prev_v_w_i = curr_v_w_i.copy()
+            prev_p_w_i = curr_p_w_i.copy()
+            # print(curr_p_w_i)
+        prev_timestamp = ts_ns
+
         if (i % skip_num == 0):
-            tm_gt.add_transform("p" + str(i), "o", pose_mat.copy())
-            white_list.append("p" + str(i))
+            pose_name = "p" + str(i)
+            tm_gt.add_transform(pose_name, "o", pose_mat.copy())
+            tm_imu_integral.add_transform(pose_name, "o", current_pose.copy())
+            white_list.append(pose_name)
 
     ll = 2
     fig = plt.figure(figsize=(10, 5))
@@ -48,11 +90,11 @@ def main():
     ax0.set_zlim((-ll, ll))
     ax0.view_init(elev=0, azim=0, vertical_axis='y')
 
-    tm_gt.plot_frames_in("p0",
-                         s=ll / 10,
-                         show_name=False,
-                         whitelist=white_list,
-                         ax=ax1)
+    tm_imu_integral.plot_frames_in("p0",
+                                   s=ll / 10,
+                                   show_name=False,
+                                   whitelist=white_list,
+                                   ax=ax1)
     ax1.set_xlim((-ll, ll))
     ax1.set_ylim((-ll, ll))
     ax1.set_zlim((-ll, ll))
